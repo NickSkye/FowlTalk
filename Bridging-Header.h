f4ca7021ca827c77addf9086cabaf56084b30dae
NickSkye <nickskye7@gmail.com>
2018-03-03 19:58:23 -0800

    init push

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  FowlTalk
+//
+//  Created by Nick Hoyt on 3/3/18.
+//  Copyright © 2018 IntelliSkye. All rights reserved.
+//
+#import <JSQMessagesViewController/JSQMessages.h>
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+
+#endif /* Bridging_Header_h */
---
+++ Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  FowlTalk
+//
+//  Created by Nick Hoyt on 3/3/18.
+//  Copyright © 2018 IntelliSkye. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Constants
+{
+    struct refs
+    {
+        static let databaseRoot = Database.database().reference()
+        static let databaseChats = databaseRoot.child("chats")
+    }
+}
